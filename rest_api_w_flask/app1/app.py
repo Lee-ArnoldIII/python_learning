0e2471e32ea2b93ee8328466c7e4d6f4b9958376
Lee Arnold <lee.arnoldiii@gmail.com>
2020-07-25 17:45:47 -0500

    added new course for developing RESTful APIs with python and flask

---
+++ rest_api_w_flask/app1/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+    'name': 'My Wonderful Store',
+        'items': [
+            {
+            'name': 'My Item',
+            'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if name == stores['name']:
+            return jsonify(store)
+        else:
+            raise jsonify({'message': 'store not found'})
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name>/item {name: price}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.json()
+    new_item = {
+        'name': request_data['name'],
+        'price': request_data['price']
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    for store in stores:
+        if stores['name'] == name:
+            return jsonify({'items': store['item']})
+        else jsonify({'message': 'store not found'})
+
+
+app.run(port=5000)
