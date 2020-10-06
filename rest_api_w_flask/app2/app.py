11533fcfe88443c88d2a944c7f2ab26d750b7c32
Lee Arnold <lee.arnoldiii@gmail.com>
2020-10-05 19:22:15 -0500

    Updating API functions and creating new routes for REST.

--- rest_api_w_flask/app2/app.py
+++ rest_api_w_flask/app2/app.py
@@ -1,14 +1,54 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'lee'
 api = Api(app)
 
-class Student(Resource):
+jwt = JWT(app, authenticate, identity)
+
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+    
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name '{name}' already exits."}, 400
 
-api.add_resource(Student, '/student/<string:name>')
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items), None)
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None: 
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
