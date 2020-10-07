58b84d15198e5ee30d430ee5c015181fec63b770
Lee Arnold <lee.arnoldiii@gmail.com>
2020-10-07 08:29:36 -0500

    updated api learning to include reqparse and parsing in requests.

--- rest_api_w_flask/app2/app.py
+++ rest_api_w_flask/app2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -14,6 +14,13 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+            )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +30,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f"An item with name '{name}' already exits."}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -34,7 +42,8 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None: 
             item = {'name': name, 'price': data['price']}
