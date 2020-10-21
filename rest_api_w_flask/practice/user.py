a6280728b1de951cc7dab10d28716e7b2111f49d
Lee Arnold <lee.arnoldiii@gmail.com>
2020-10-21 17:26:13 -0500

    added new files for next stage of tutorial

---
+++ rest_api_w_flask/practice/user.py
@@ -0,0 +1,8 @@
+class User():
+    def __init__(self, _id, f_name, l_name, username, password):
+        self.id = _id
+        self.f_name = f_name
+        self.l_name = l_name
+        self.username = username
+        self.password = password
+
