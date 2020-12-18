7987c057ed56f44a2dba645cb43fb5b55c9621ab
Lee Arnold <lee.arnoldiii@gmail.com>
2020-12-17 20:42:46 -0600

    added SQLAlchemy to project. still have some changes to make for UserModel and ItemList. Continue updating tomorrow.

--- rest_api_w_flask/app4/app.py
+++ rest_api_w_flask/app4/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from db import db
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'lee'
 api = Api(app)
 
@@ -17,4 +20,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
--- rest_api_w_flask/app4/resources/item.py
+++ rest_api_w_flask/app4/resources/item.py
@@ -27,21 +27,16 @@ class Item(Resource):
         item = ItemModel(name, data['price'])
 
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {"message": "An error occurred insurting the item."}, 500
 
         return item.json(), 201
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
 
         return {'message': 'Item deleted'}
 
@@ -49,19 +44,16 @@ class Item(Resource):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
+        
 
         if item is None: 
-            try:
-                updated_item.insert()
-            except: 
-                return {"message": "An error occurred inserting the item."}, 500
+            item = ItemModel(name, data['price'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {"message": "An error occurred updating the item."}, 500
-        return updated_item.json()
+            item.price = data['price']
+       
+        item.save_to_db()
+
+        return item.json()
 
 
 class ItemList(Resource):
