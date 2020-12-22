298df581f3be6346adcbd3a5ebcc00ce1680f698
Lee Arnold <lee.arnoldiii@gmail.com>
2020-12-22 16:30:51 -0600

    final updats for api via sqlalchelmy and sqlite. changed table structure, routes added for store and store list, and additional changes.

--- rest_api_w_flask/app4/app.py
+++ rest_api_w_flask/app4/app.py
@@ -6,6 +6,7 @@ from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -19,8 +20,11 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
