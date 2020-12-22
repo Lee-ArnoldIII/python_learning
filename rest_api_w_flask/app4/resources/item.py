6dc60bfbab8e2b61dd44d0558c80870834b40baf
Lee Arnold <lee.arnoldiii@gmail.com>
2020-12-22 15:21:23 -0600

    included StoreModel in updating file structure and how API works.

--- rest_api_w_flask/app4/resources/item.py
+++ rest_api_w_flask/app4/resources/item.py
@@ -8,7 +8,12 @@ class Item(Resource):
             type=float,
             required=True,
             help="This field cannot be left blank!"
-            )
+    )
+    parser.add_argument('store_id',
+            type=int,
+            required=True,
+            help="Every item needs a store id!"
+    )
 
     @jwt_required()
     def get(self, name):
@@ -23,7 +28,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -45,9 +50,10 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
  
         if item is None: 
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
+            item.store_id = data['store_id']
        
         item.save_to_db()
 
@@ -56,5 +62,5 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'itmes': [item.json() for item in ItemModel.query.all()]}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
         # alternative using lambda => return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
